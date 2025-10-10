5a3a95a07024597b47bdbeb6e2717b70f63cbfb3
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-10-09 21:04:38 -0300

    Adicionadas TBCategoria, TBDespesa e TBCategoria_TBDespesa

--- eAgenda.Infraestrutura.Orm/AppDbContext.cs
+++ eAgenda.Infraestrutura.Orm/AppDbContext.cs
@@ -1,7 +1,11 @@
+using eAgenda.Dominio.ModuloCategoria;
 using eAgenda.Dominio.ModuloCompromisso;
 using eAgenda.Dominio.ModuloContato;
+using eAgenda.Dominio.ModuloDespesa;
+using eAgenda.Infraestrutura.Orm.ModuloCategoria;
 using eAgenda.Infraestrutura.Orm.ModuloCompromisso;
 using eAgenda.Infraestrutura.Orm.ModuloContato;
+using eAgenda.Infraestrutura.Orm.ModuloDespesa;
 using Microsoft.EntityFrameworkCore;
 
 namespace eAgenda.Infraestrutura.Orm;
@@ -10,11 +14,15 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Contato> Contatos { get; set; }
     public DbSet<Compromisso> Compromissos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Despesa> Despesas { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new MapeadorContatoEmOrm());
         modelBuilder.ApplyConfiguration(new MapeadorCompromissoEmOrm());
+        modelBuilder.ApplyConfiguration(new MapeadorCategoriaEmOrm());
+        modelBuilder.ApplyConfiguration(new MapeadorDespesaEmOrm());
 
         base.OnModelCreating(modelBuilder);
     }
